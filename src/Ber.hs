a66ba6aec36c962d802b7d8dd4457ee74d1d921a
Mateusz Biel <matbiel@gmail.com>
2018-12-02 01:26:42 +0100

    update

--- src/Ber.hs
+++ src/Ber.hs
@@ -32,6 +32,36 @@ _encodeInteger = removeRedundancy . B.unpack . Binary.encode
 
 skipLeadingZeroes = dropWhile (\x -> x == 0) . B.unpack . Binary.encode
 
+lastBit = (flip rotate) (-7) . (.&.) 0x80
+
+getNFirst :: Int -> Word8 -> Word8
+getNFirst n w = (rotate 0xFF (-8+n)) .&. w
+
+getNLast :: Int -> Word8 -> Word8
+getNLast n w = (flip rotate) (-8+n) ((rotate 0xFF (8-n)) .&. w)
+
+blend :: (Int, Word8) -> Word8 -> ((Int, Word8), Word8)
+blend (v, w) a = ((carryBits, getNLast carryBits a), ((flip rotate) v $ getNFirst toTake a) .|. w)
+    where toTake = 7 - v
+          carryBits = 8 - toTake
+
+_encodeOnSeven :: [Word8] -> (Int, Word8) -> [Word8] -> [Word8]
+_encodeOnSeven [] p a | snd p == 0 = a
+                      | fst p > 0 = (snd p):a
+                      | otherwise = a
+_encodeOnSeven (x:xs) p a | fst p == 7 = _encodeOnSeven (x:xs) (0,0) ((snd p):a)
+                          | fst p > 0 = let (u, w) = blend p x in  _encodeOnSeven xs u (w:a)
+                          | otherwise = _encodeOnSeven xs (1, lastBit x) ((x .&. 0x7F):a)
+
+encodeOnSeven w = _encodeOnSeven w (0,0) []
+
+markBytes :: [Word8] -> Bool -> [Word8] -> [Word8]
+markBytes a _ [] = a
+markBytes a True (x:xs) = markBytes (x:a) False xs
+markBytes a False (x:xs) = markBytes ((x .|. 0x80):a) False xs
+
+encodeIntegerOnSeven = markBytes [] True . reverse . encodeOnSeven . reverse . skipLeadingZeroes
+
 encodeLongLen :: Int -> PutM ()
 encodeLongLen l = do
     let value = skipLeadingZeroes l
