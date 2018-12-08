4e31462274bcd6a3f362e5e35f61564b7dd60e58
Mateusz Biel <matbiel@gmail.com>
2018-12-08 16:25:14 +0100

    small refactor

--- test/BerSpec.hs
+++ test/BerSpec.hs
@@ -11,20 +11,22 @@ import Ber
 cToW8 :: Char -> Word8
 cToW8 = (fromIntegral :: Int -> Word8) . ord
 
+encoded = B.unpack . encode
+
 berSpec = do
             describe "encode" $ do
                 it "should encode primitive integer type" $ do
                     let v = IntegerValue 5
-                    (B.unpack $ encode v) `shouldBe` [1, 5]
+                    encoded v `shouldBe` [1, 5]
 
                 it "should encode encode octet string" $ do
                     let v = StringValue "xvi"
-                    (B.unpack $ encode v) `shouldBe` [3, cToW8 'x', cToW8 'v', cToW8 'i']
+                    encoded v `shouldBe` [3, cToW8 'x', cToW8 'v', cToW8 'i']
 
                 it "should encode simple OID" $ do
                     let v = ObjectIdentifier [1,2,3]
-                    (B.unpack $ encode v) `shouldBe` [2, 42, 3]
+                    encoded v `shouldBe` [2, 42, 3]
 
                 it "should encode OID with big number" $ do
                     let v = ObjectIdentifier [1,2,500]
-                    (B.unpack $ encode v) `shouldBe` [3, 42, 0x83, 0x74]
+                    encoded v `shouldBe` [3, 42, 0x83, 0x74]
